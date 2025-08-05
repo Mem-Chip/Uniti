687adbe4bc8eb129e2d744cd944e509be7fe3b1d
likectrl2 <likectrl2@gmail.com>
2025-08-06 00:57:43 +0800

    feat: add rescources for entity

--- Assets/Scripts/Game/entity/EntityData.cs
+++ Assets/Scripts/Game/entity/EntityData.cs
@@ -1,8 +1,31 @@
 using UnityEngine;
 
+public struct Resource<T> {
+    T max;
+    T now;
+
+    public Resource(T max)
+    {
+        this.max = max;
+        this.now = max;
+    }
+    public Resource(T max, T now)
+    {
+        this.max = max;
+        this.now = now;
+    }
+}
+
+public struct Resources {
+    Resource<int> action;
+    Resource<float> speed;
+}
+
 public struct EntityData
 {
     public int health;
+    public int healthMax;
     public GridPosition position;
     public string entityName;
+    public Resources resources;
 }

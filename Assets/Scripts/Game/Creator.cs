045c5723c86123cf73fb71e4ba210f9f4ab09919
likectrl2 <likectrl2@gmail.com>
2025-08-06 23:43:03 +0800

    feat: rename

--- Assets/Scripts/Game/Creator.cs
+++ Assets/Scripts/Game/Creator.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public static class Creator
 {
-    public static GameObject CreateNext<ItemType, DataType>(ItemType emptyItem, DataType data) where ItemType : ICreatable<DataType>
+    public static GameObject Create<ItemType, DataType>(ItemType emptyItem, DataType data) where ItemType : ICreatable<DataType>
     {
         GameObject entity = Object.Instantiate(Resources.Load<GameObject>(emptyItem.PREFABPATH));   //用预制体生成实例
         return entity.GetComponent<ItemType>().Initialize(data);

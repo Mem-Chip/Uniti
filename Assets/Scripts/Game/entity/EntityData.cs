ca112b8a6b655e76abef788557ed243f9a9ea4eb
likectrl2 <likectrl2@gmail.com>
2025-08-06 22:38:18 +0800

    feat: add construct func

--- Assets/Scripts/Game/entity/EntityData.cs
+++ Assets/Scripts/Game/entity/EntityData.cs
@@ -1,7 +1,21 @@
 public struct EntityData
 {
+    public GamePosition position;
+    public string entityName;
+
     public Stat<int> health;
     public int initiative;
-    public GridPosition position;
-    public string entityName;
+
+    public EntityData(
+        Stat<int> health,
+        string entityName = "",
+        GamePosition position = new GamePosition(),
+        int initiative = 10
+    )
+    {
+        this.health = health;
+        this.entityName = entityName;
+        this.position = position;
+        this.initiative = initiative;
+    }
 }
